73ab4b14af1d4a08afea82580f08a49eef30df17
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-05-03 15:39:24 -0500

    Added board accessor

--- src/mancala.h
+++ src/mancala.h
@@ -1,3 +1,11 @@
+#include <array>
+
 namespace mancala {
-  class Board {};
+  class Board {
+    std::array<int, 14> pockets;
+
+    int operator[] (int n) {
+      return pockets[n];
+    }
+  };
 }  // namespace mancala
