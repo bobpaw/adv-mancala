db3fa03aef0bb5bb44431193f883ed5219774be9
splaark <78822286+splaark@users.noreply.github.com>
2021-05-12 16:13:56 -0500

    Add basic game loop

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,26 @@
+#include <iostream>
 #include "main.h"
 
-int main () {
-	return 0;
+int main() {
+
+  //let the player pick the ruleset
+  std::cout << "Which ruleset would you like to use? \n  0: Capture\n  1: Avalanche"<<std::endl;
+  int ruleset;
+  std::cin >> ruleset;
+
+  std::cout << "Starting Game..."<<std::endl;
+
+  mancala::Board game; // im not sure how you would initialize the ruleset
+
+  //game loop
+  bool endGame = false;
+  while(endGame == false) {
+    //ask player which # to move
+    std::cout<<"Please make your move: ";
+    int moveNum;
+    std::cin >> moveNum;
+
+    game.move(moveNum);
+  }
+  
 }
