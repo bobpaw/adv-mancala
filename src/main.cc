bc04dc8a9ba17ce17426b0eef4345aeb03b3485d
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-05-12 17:24:03 -0500

    Added board output
    Switch player number

--- src/main.cc
+++ src/main.cc
@@ -13,23 +13,24 @@ int main() {
 
 	std::cout << "Starting Game..." << std::endl;
 
-	mancala::Board game;  // im not sure how you would initialize the ruleset
+	mancala::Board game;
 	game.rules = mancala::Board::Ruleset(ruleset);
 
 	for (unsigned i = 1; i < 7; ++i) {
 		game[i] = 6;
-		game[14 - i] = 6;
+		game[7 + i] = 6;
 	}
 
 	// game loop
 	bool endGame = false;
 	while (!endGame) {
 		// ask player which # to move
-		std::cout << "Please make your move: ";
-		int moveNum;
+		std::cout << game << std::endl;
+		std::cout << "Player " << game.player + 1 << ", please make your move (1-6): ";
+		unsigned moveNum;
 		std::cin >> moveNum;
 
-		game.move(moveNum);
+		if (moveNum - 1 < 6) game.move(game.player * 7 + moveNum);
 	}
 
 	return 0;
