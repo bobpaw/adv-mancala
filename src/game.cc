ffa6724fffc7c67ec38333eeea7f868fee4c1010
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-06-07 08:52:37 -0500

    Don't really need game.cc since MoveInfo fulfills the role

--- src/game.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "game.h"
-
-int main() {
-	std::cout << "sizeof(GameFrame): " << sizeof(mancala::GameFrame) << std::endl;
-	std::cout << "sizeof(MoveFrame): " << sizeof(mancala::GameFrame::MoveFrame) << std::endl;
-	std::cout << "sizeof(Board): " << sizeof(mancala::Board) << std::endl;
-}
